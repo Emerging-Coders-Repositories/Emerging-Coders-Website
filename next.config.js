146762a75ccd96d5904fd46a8afd9bed41009d81
Ethan Pineda <joshuapineda66@gmail.com>
2024-11-04 11:44:47 -0600

    lets see if this works

--- next.config.js
+++ next.config.js
@@ -1,16 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
   output: "export",
   images: {
     unoptimized: true,
-    loader: "akamai",
-    path: "",
   },
   basePath:
     process.env.NODE_ENV === "production" ? "/Emerging-Coders-Website" : "",
   assetPrefix:
-    process.env.NODE_ENV === "production" ? "/Emerging-Coders-Website/" : "",
+    process.env.NODE_ENV === "production" ? "/Emerging-Coders-Website" : "",
   trailingSlash: true,
 };
 
