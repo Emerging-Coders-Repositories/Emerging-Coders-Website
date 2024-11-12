b6851900fc7053b002c708a09c73d37f720d8e31
desmondyayra <desmondadanuty2025@u.northwestern.edu>
2024-11-12 00:52:38 -0600

    update jeb exec for 2024 and next.config.js to enable local hosting to work

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
+  
   output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: "/Emerging-Coders-Website",
-  assetPrefix: "/Emerging-Coders-Website",
+  basePath: isProd ? "/Emerging-Coders-Website" : '',
+  assetPrefix: isProd ? "/Emerging-Coders-Website" : '',
   trailingSlash: true,
 };
 
