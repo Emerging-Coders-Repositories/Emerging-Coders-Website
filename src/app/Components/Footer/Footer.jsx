de1bab32aec4a7608baad22fbf839dbd34d349f5
desmondyayra <desmondadanuty2025@u.northwestern.edu>
2024-11-25 20:18:50 -0600

    changing footer text to current year

--- src/app/Components/Footer/Footer.jsx
+++ src/app/Components/Footer/Footer.jsx
@@ -37,6 +37,8 @@ export default function Footer() {
         },
     ]
 
+    const currentYear = new Date().getFullYear();
+
     return (
     <footer class="p-4 bg-gray-800 md:p-8 lg:p-10 dark:bg-gray-800 z-100">
         <div class="mx-auto max-w-screen-xl text-center z-100">
@@ -51,7 +53,7 @@ export default function Footer() {
             ))}
         </ul>
         <div class="flex flex-col justify-center mt-6 space-x-4 gap-2">
-            <span class="text-sm text-white sm:text-center dark:text-gray-400">© 2023 <a href="#" class="hover:underline">Emerging Coders™</a>. All Rights Reserved.</span>
+            <span class="text-sm text-white sm:text-center dark:text-gray-400">© {currentYear} <a href="#" class="hover:underline">Emerging Coders™</a>. All Rights Reserved.</span>
             <span class="text-sm text-white sm:text-center dark:text-gray-400">Built and Designed By: <a href="https://www.linkedin.com/in/ethanpineda/" target="blank" class="hover:underline">Ethan Pineda</a></span>
             <span class="text-sm text-white sm:text-center dark:text-gray-400">This project is open source, contribute <a href="https://github.com/ethanpaneraa/Emerging-Coders-Website" target="blank" class="hover:underline">here</a></span>
         </div>
