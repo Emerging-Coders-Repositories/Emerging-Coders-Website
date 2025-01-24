28181ef2ad53e5070f4cdefdbc5ae79bab1d0823
desmondyayra <desmondadanuty2025@u.northwestern.edu>
2025-01-24 16:59:09 -0600

    Revert "correct next config for custom domain"
    This reverts commit d283dc2ab6ed7b1045ffad01fd091a827e838df7.

--- next.config.js
+++ next.config.js
@@ -1,15 +1,14 @@
 /** @type {import('next').NextConfig} */
 
 const isProd = process.env.NODE_ENV === 'production';
-const hasCustomDomain = process.env.CUSTOM_DOMAIN === 'true';
-
 const nextConfig = {
+  
   output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: isProd && !hasCustomDomain ? "/Emerging-Coders-Website" : '',
-  assetPrefix: isProd && !hasCustomDomain ? "/Emerging-Coders-Website" : '', 
+  basePath: isProd ? "/Emerging-Coders-Website" : '',
+  assetPrefix: isProd ? "/Emerging-Coders-Website" : '',
   trailingSlash: true,
 };
 
