d283dc2ab6ed7b1045ffad01fd091a827e838df7
desmondyayra <desmondadanuty2025@u.northwestern.edu>
2025-01-24 01:35:24 -0600

    correct next config for custom domain

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 
 const isProd = process.env.NODE_ENV === 'production';
+const hasCustomDomain = process.env.CUSTOM_DOMAIN === 'true';
+
 const nextConfig = {
-  
   output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: isProd ? "/Emerging-Coders-Website" : '',
-  assetPrefix: isProd ? "/Emerging-Coders-Website" : '',
+  basePath: isProd && !hasCustomDomain ? "/Emerging-Coders-Website" : '',
+  assetPrefix: isProd && !hasCustomDomain ? "/Emerging-Coders-Website" : '', 
   trailingSlash: true,
 };
 
