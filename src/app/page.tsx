eb6675719f57e045411ed50993f9f3a8b6278bd3
Ethan Pineda <joshuapineda66@gmail.com>
2025-03-26 15:53:04 -0500

    fix: fixing mobile view for about us photo grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
   return (
     <div className="min-h-screen flex flex-col">
       <Hero />
-      {/* <AboutusPhotoGrid /> */}
+      <AboutusPhotoGrid />
       <AboutusCards />
       <PastEventsSection />
       <MeetTheExec />
--- src/components/landing/about/about-us-photos.tsx
+++ src/components/landing/about/about-us-photos.tsx
@@ -15,15 +15,15 @@ export default function AboutusPhotoGrid() {
   };
 
   return (
-    <div className="bg-black py-24 sm:py-32 min-h-screen min-w-10/12 mx-auto">
-      <div className="container mx-auto px-6">
-        <div className="flex flex-col lg:flex-row">
-          <div className="lg:w-7/12 mb-10 lg:mb-">
-            <div className="lg:top-8 flex justify-center flex-col items-center">
+    <div className="bg-black py-24 sm:py-32 min-h-screen">
+      <div className="max-w-[110rem] mx-auto px-8">
+        <div className="flex flex-col items-center justify-center lg:flex-row lg:gap-32">
+          <div className="lg:w-6/12 mb-10 lg:mb-0">
+            <div className="lg:sticky lg:top-8 flex flex-col">
               <h2 className="text-5xl font-semibold -tracking-4 mb-6 text-white">
                 About Emerging Coders
               </h2>
-              <p className="font-mono text-sm leading-relaxed text-zinc-400 mb-4 w-lg">
+              <p className="font-mono text-sm leading-relaxed text-zinc-400 mb-4">
                 Emerging Coders is a community for FGLI (First-generation,
                 Low-income) students who are interested in Tech. Our purpose is
                 to enable students in the club improve their skills (e.g.,
@@ -31,16 +31,16 @@ export default function AboutusPhotoGrid() {
                 resources throughout your college experience, and to create
                 meaningful projects.
               </p>
-              <p className="font-mono text-sm leading-relaxed text-zinc-400 mb-6 w-lg">
+              <p className="font-mono text-sm leading-relaxed text-zinc-400 mb-6">
                 We host workshops, networking events, and provide resources to
                 help students succeed in their tech careers. Join us to connect
                 with peers who share similar backgrounds and aspirations.
               </p>
             </div>
           </div>
-          <div className="w-6/12 mx-auto">
+          <div className="lg:w-6/12 w-full">
             <div className="flex flex-col gap-4 lg:hidden">
-              {images.map((image) => (
+              {images.slice(0, 4).map((image) => (
                 <div
                   key={`mobile-${image.id}`}
                   className="relative overflow-hidden rounded-2xl border border-neutral-800 bg-neutral-950 group"
