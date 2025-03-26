3fb1bd8002cb33bf3fed12701375a86a40e7d284
Ethan Pineda <joshuapineda66@gmail.com>
2025-03-26 14:55:23 -0500

    fix: changing css on the marquee again'

--- src/components/testimonial-card.tsx
+++ src/components/testimonial-card.tsx
@@ -16,7 +16,7 @@ export default function TestimonialCard({
 }: TestimonialCardProps) {
   return (
     <div className="relative my-1 p-8 rounded-xl border border-zinc-800 bg-neutral-950  hover:bg-gradient-to-br hover:from-purple-600 hover:via-purple-500/50 hover:to-fuchsia-500/40 transition-colors duration-300 w-full">
-      <div className="flex flex-col h-full justify-between min-h-[200px]">
+      <div className="flex flex-col h-full justify-between min-h-[250px]">
         <p className="text-base text-white leading-relaxed font-mono mb-6">
           {body}
         </p>
--- src/components/testimonials-marquee.tsx
+++ src/components/testimonials-marquee.tsx
@@ -6,7 +6,7 @@ export default function EmergingCodersTestimonials(): React.ReactElement {
   const columns = createColumns();
 
   return (
-    <div className="w-full 4xl:max-w-9/12 mx-auto h-[150vh] text-white">
+    <div className="w-full 4xl:max-w-8/12 mx-auto h-[150vh] text-white">
       <div className="h-full flex flex-col w-full 3xl:mx-auto">
         <div className="text-center py-6">
           <h2 className="text-5xl font-bold mb-4">What Our Members Say</h2>
@@ -16,9 +16,9 @@ export default function EmergingCodersTestimonials(): React.ReactElement {
           </p>
         </div>
 
-        <div className="relative flex-1 flex justify-between w-full px-4 min-h-0">
+        <div className="relative flex-1 flex justify-between w-full px-2 min-h-0">
           {columns.map((column, i) => (
-            <div key={`col-${i}`} className="flex-1 max-w-md mx-2">
+            <div key={`col-${i}`} className="max-w-md">
               <Marquee
                 className={`h-full [--gap:0.5rem] [--duration:85s]`}
                 pauseOnHover
@@ -49,13 +49,13 @@ export default function EmergingCodersTestimonials(): React.ReactElement {
 
           <div className="pointer-events-none absolute inset-0 z-10">
             {/* Left gradient */}
-            <div className="absolute inset-y-0 left-0 w-50 bg-gradient-to-r from-black via-black to-transparent"></div>
+            <div className="absolute inset-y-0 left-0 w-90 bg-gradient-to-r from-black via-black to-transparent"></div>
             {/* Right gradient */}
-            <div className="absolute inset-y-0 right-0 w-50 bg-gradient-to-l from-black via-black to-transparent"></div>
+            <div className="absolute inset-y-0 right-0 w-90 bg-gradient-to-l from-black via-black to-transparent"></div>
             {/* Top gradient */}
-            <div className="absolute inset-x-0 top-0 h-24 bg-gradient-to-b from-black"></div>
+            <div className="absolute inset-x-0 top-0 h-36 bg-gradient-to-b from-black"></div>
             {/* Bottom gradient */}
-            <div className="absolute inset-x-0 bottom-0 h-72 bg-gradient-to-t from-black via-black to-transparent"></div>
+            <div className="absolute inset-x-0 bottom-0 h-84 bg-gradient-to-t from-black via-black to-transparent"></div>
           </div>
         </div>
       </div>
